f513eb643c165fc440168f627390da7c7bf6d036
KatsimonY <139374446+KatsimonY@users.noreply.github.com>
2023-11-06 19:47:31 +0200

    Delete src directory

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const control = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-
-control.addEventListener("input", controlEvent);
-
-function controlEvent(event) {
-  text.style.fontSize = `${event.currentTarget.value}px`;
-}
